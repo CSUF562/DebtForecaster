8eaece97da2f52694903110f2dd3c58891341284
CSUF562 <Joelxr726@gmail.com>
2026-09-19 02:01:13 -0400

    Fix Next.js TypeScript import resolution in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { getDebtSnapshot } from "../src/application/debtSnapshot.js";
-import { buildDailyAccountingBrief } from "../src/application/dailyBrief.js";
-import { buildSeptember2026ContextBundle } from "../src/context/currentCaseStudy.js";
+import { getDebtSnapshot } from "../src/application/debtSnapshot";
+import { buildDailyAccountingBrief } from "../src/application/dailyBrief";
+import { buildSeptember2026ContextBundle } from "../src/context/currentCaseStudy";
 
 export const dynamic = "force-dynamic";
 
